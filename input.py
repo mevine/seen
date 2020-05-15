44e300fb94032f6d56cbcea4ef87acae2213dad8
Germaine <sky@localhost.localdomain>
2020-05-15 15:20:30 +0300

    input.py

---
+++ input.py
@@ -0,0 +1,47 @@
+#data = input("what`s your favorite colour?")
+#print("you said" + data)
+
+
+#print("what`s your favorite colour?")
+#data = input()
+#print("you said" + "" + data)
+
+name=input("whats your name")
+if name =="arya stale":
+    print("barbara morgans")
+elif name =="jon snow":
+    print("you know nothing")
+elif name ==("jame sweezy"):
+    print("woow!")  
+elif name == ("kael kelsey"): 
+    print("ombwedede")    
+else:
+    print("carry on")
+
+#city=input("where do you live") 
+#if city=="los angeles"or"san francisco":
+    #print("you live in carlifonia")
+#else:
+    #print("you live elsewhere") 
+
+
+
+#age = 70
+#if not((age >= 2 and age <= 8 ) or age >=65 ):
+    #print("you pay 100 shillings") 
+    
+#else:
+    #print("you weeeennnnn!") 
+
+age = input("how old are you: ") 
+if  age :                                                        #ask age
+    if  int(age) >= 18 and int (age) < 21:
+       print("you can enter but need a wrist band")                                            #18-21 wear wristband
+    elif int (age) >= 21:
+       print("you can enter and have drinks") 
+    else:
+       print("you cant come in little one")           
+                                                       #21+ drink,normal entry
+else:
+    print("enter your age")                                                           #too young
+    
