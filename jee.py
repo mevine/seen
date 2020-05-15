9e9a3fe7d111c0982ba23103b00ebff134b43f0c
Germaine <sky@localhost.localdomain>
2020-05-15 15:36:37 +0300

    jee.py

---
+++ jee.py
@@ -0,0 +1,82 @@
+#for X in range(1,10):
+    #print(X)
+
+#for char in "cofee":
+        #print(char * 10)
+
+#for num in range (0,20,2):#if you start with odd nums it will print odd(1,20,2)
+    #print(num)
+
+#times = input("how many times do i have to tell you? ") 
+#times = int(times)
+#for time in range(times) :
+   # print ("clean up your room!") 
+
+
+#for num in range(1,21):
+   #if num ==4 or num ==13:
+       #print(f"{num} is unlucky")
+   #elif num %  2==0:
+      #print(f"{num} is even") 
+  # else:
+      #print(f"{num} is odd")
+
+
+
+#for num in range(1,21):
+   #if num ==4 or num ==13:
+       #print(f"{num} is unlucky")
+   #elif num %  2==0:
+      #print(f"{num} is even") 
+   #else:
+      #state() 
+      #print(f"{num} is odd") 
+
+
+
+#while loop
+#msg = input("whats your password?")
+#while msg != "bananas":
+    #print("wrong!")
+    #msg = input("whats your password?") 
+#print("correct!") 
+
+#num =1
+#while num < 11:
+    #print(num)
+    #num += 1  
+
+
+#for num in range(1,11) :
+    #print(" \U0001f600" * num)
+
+#times = 1
+#while times < 11:
+    #print("       \U0001f600" * times) 
+    #times += 1  
+
+
+#msg = input("say something: ") 
+#while msg != "stop copying me":
+    #print(msg)
+    #msg = input() 
+#print("you win!")  
+
+
+while True:
+    command = input("type  'exit' to exit:")
+    if (command == "exit"):
+        break
+
+
+
+
+times = int(input("how many times do i have to tell yah?"))
+for time in range(times):
+    print("claen your room!")
+    if time >= 3:
+        print("do you even listen anymore")
+        break
+
+
+      
